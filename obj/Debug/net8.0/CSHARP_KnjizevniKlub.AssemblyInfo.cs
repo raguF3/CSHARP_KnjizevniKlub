26db3e72dd8ad418e551c8b1711e28e2217aec63
Raguž Lea <raguz.leaa@gmail.com>
2024-09-26 10:29:13 +0200

    .gitignore

--- obj/Debug/net8.0/CSHARP_KnjizevniKlub.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSHARP_KnjizevniKlub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSHARP_KnjizevniKlub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1936f558caec6055dc41344f1cc735111c6519a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSHARP_KnjizevniKlub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSHARP_KnjizevniKlub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
