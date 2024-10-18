b22cae76c13dbfae34c82b77241d06d9071a088c
Raguž Lea <raguz.leaa@gmail.com>
2024-10-18 23:01:39 +0200

    navbar

--- Frontend/src/components/NavBarKnjizevniKlub.jsx
+++ Frontend/src/components/NavBarKnjizevniKlub.jsx
@@ -5,45 +5,32 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { RoutesNames } from '../constants';
 import { useNavigate } from 'react-router-dom';
 
+export default function NavBarKnjizevniKlub() {
+  const navigate = useNavigate();
 
-
-
-export default function NavBarKnjizevniKlub(){
-
-const navigate = useNavigate();
-
-
-    return(
-
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Književni Klub</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link onClick={()=>navigate(RoutsNames.HOME)}>Početna</Nav.Link>
-              <Nav.Link href="https://raguf3-001-site1.ktempurl.com/swagger/index.html" target='_blank'>Swagger</Nav.Link>
-              <NavDropdown title="Sadržaj" id="basic-nav-dropdown">
-                <NavDropdown.Item onClick={()=>navigate(RoutsNames.DOLAZAK_PREGLED)}>Dolasci</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-    );
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Književni Klub</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => navigate(RoutesNames.HOME)}>Početna</Nav.Link>
+            <Nav.Link href="https://raguf3-001-site1.ktempurl.com/swagger/index.html" target='_blank'>Swagger</Nav.Link>
+            <NavDropdown title="Sadržaj" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={() => navigate(RoutesNames.DOLAZAK_PREGLED)}>Dolasci</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate(RoutesNames.CLANOVI)}>Članovi</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate(RoutesNames.SASTANCI)}>Sastanci</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate(RoutesNames.KNJIGE)}>Knjige</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link eventKey={2} href="#memes"></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
